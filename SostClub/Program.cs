e568b6abbdf74b5800a499259ea4424bc22f7040
Oybek Nematov <1377oybeknematov1377@gmail.com>
2025-06-19 03:06:32 +0500

    Car class

--- SostClub/Program.cs
+++ SostClub/Program.cs
@@ -1,60 +1,61 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
-public class Computer
+public class Car
 {
     public string Brend { get; set; }
-    public string Protsessor { get; set; }
-    public int Ram { get; set; }
-    public int Disk { get; set; }
+    public string Name { get; set; }
+    public int Speed { get; set; }
+    public int Price { get; set; }
 
-    public Computer(string brend, string protsessor, int ram, int disk)
+    public Car(string brend, string name, int speed, int price)
     {
         Brend = brend;
-        Protsessor = protsessor;
-        Ram = ram;
-        Disk = disk;
+        Name = name;
+        Speed = speed;
+        Price = price;
     }
 
     public virtual void ShowInfo()
     {
         Console.WriteLine($"Brend: {Brend}");
-        Console.WriteLine($"Protsessor: {Protsessor}");
-        Console.WriteLine($"RAM: {Ram} GB");
-        Console.WriteLine($"Disk: {Disk} GB");
+        Console.WriteLine($"Nomi: {Name}");
+        Console.WriteLine($"Tezligi: {Speed} km/h");
+        Console.WriteLine($"Narxi: {Price} $");
     }
 }
 
-public class Laptop : Computer
+public class PassangerCar : Car
 {
-    public int BatareyaQuvvati { get; set; }
+    public int Soni { get; set; }
 
-    public Laptop(string brend, string protsessor, int ram, int disk, int batareya)
-        : base(brend, protsessor, ram, disk)
+    public PassangerCar(string brend, string name, int speed, int price, int soni)
+        : base(brend, name, speed, price)
     {
-        BatareyaQuvvati = batareya;
+        Soni = soni;
     }
 
     public override void ShowInfo()
     {
         base.ShowInfo();
-        Console.WriteLine($"Batareya Uzunligi: {BatareyaQuvvati} soat");
+        Console.WriteLine($"Yo'lovchilar soni: {Soni} nafar");
     }
 }
 
-public class SmartPhone : Computer
+public class CargoCar : Car
 {
-    public bool Sensorli { get; set; }
+    public bool YukUchun { get; set; }
 
-    public SmartPhone(string brend, string protsessor, int ram, int disk, bool sensorli)
-        : base(brend, protsessor, ram, disk)
+    public CargoCar(string brend, string name, int speed, int price, bool yuk)
+        : base(brend, name, speed, price)
     {
-        Sensorli = sensorli;
+        YukUchun = yuk;
     }
 
     public override void ShowInfo()
     {
         base.ShowInfo();
-        Console.WriteLine($"Sensorli ekran: {(Sensorli ? "Ha" : "Yo'q")}");
+        Console.WriteLine($"Faqat yuk uchun: {(YukUchun ? "Ha" : "Yo'q")}");
     }
 }
 
@@ -62,15 +63,15 @@ class Program
 {
     static void Main()
     {
-        Computer pc = new Computer("HP", "Intel i5", 8, 512);
-        Laptop laptop = new Laptop("Dell", "Intel i7", 16, 1024, 10);
-        SmartPhone phone = new SmartPhone("Samsung", "Snapdragon", 6, 128, true);
+        Car car = new Car("Mersedes", "Mers", 200, 5000);
+        PassangerCar pcar = new PassangerCar("Mersedes", "Mersedes Bus", 180, 10000, 25);
+        CargoCar cargo = new CargoCar("Isuzu", "Isuzu200", 220, 1200000, true);
 
-        Console.WriteLine("---- KOMPYUTER ----");
-        pc.ShowInfo();
-        Console.WriteLine("\n---- NOUTBUK ----");
-        laptop.ShowInfo();
-        Console.WriteLine("\n---- SMARTFON ----");
-        phone.ShowInfo();
+        Console.WriteLine("---- Car ----");
+        car.ShowInfo();
+        Console.WriteLine("\n---- PassangerCar ----");
+        pcar.ShowInfo();
+        Console.WriteLine("\n---- CargoCar ----");
+        cargo.ShowInfo();
     }
 }
